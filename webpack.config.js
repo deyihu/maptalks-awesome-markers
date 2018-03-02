b2dc3e81e832a28486b4de9a18f730458beb1c14
Iverson <mehudeyi@163.com>
2018-03-02 12:38:18 +0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-/**
- * Created by z.c. on 16/8/14.
- */
-var path = require('path');
+var PROD = process.argv.indexOf('-p') >= 0;
 var webpack = require('webpack');
-module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './index.js')],
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'maptalks-awesome-marker.js'
-    },
-    devServer: {
-        inline: true,
-        port: 8181
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
+module.exports = {
+    plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new CaseSensitivePathsPlugin({}),
+        new webpack.DefinePlugin({
+            'typeof __DEV__': JSON.stringify('boolean'),
+            __DEV__: PROD ? false : true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
             }
         ]
     },
-    plugins: [
-	     new webpack.NoErrorsPlugin(),
-
-		 // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         drop_console: true,
-        //         warnings: false
-        //     }
-        // }),
-        new webpack.HotModuleReplacementPlugin()
-    ]
+    entry: {
+        'maptalks-awesome-markers': __dirname + '/index.js'
+    },
+    output: {
+        libraryTarget: 'umd',
+        path: __dirname + '/dist',
+        filename: PROD ? '[name].min.js' : '[name].js'
+    },
+    externals: {
+        
+    }
 };
